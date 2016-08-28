0e7373f0563fb0d1c0284eef5b13c649e8adad05
kartikay101 <shandilkartikay@gmail.com>
2016-08-28 07:39:37 +0530

    #3 done

---
+++ CODING/question_3/question_3.cpp
@@ -0,0 +1,82 @@
+    #include<iostream>
+    void input();
+    void display();
+    int test();
+    using namespace std ;
+     
+    int sud[9][9];
+    int main()
+    {
+    int x=1;
+    input();
+    display();
+    x=test();
+     
+    if(x==1){
+    cout<<"Valid SUDOKU";
+    }
+    else{
+    cout<<"Invalid SUDOKU";
+    }
+     
+    }
+     
+    void input(){
+    for(int i=0;i<9;i++)
+    {
+    for(int j=0;j<9;j++)
+    {
+    cin>>sud[i][j];
+    }
+    //cout<<"\n\n";
+    }
+    }
+    void display(){
+    for(int i=0;i<9;i++)
+    {
+    for(int j=0;j<9;j++)
+    {
+    cout<<sud[i][j]<<" ";
+    }
+    cout<<"\n";
+    }
+    //cout<<"\n\n";
+    }
+     
+    int test(){
+    int a[]={0,0,0,0,0,0,0,0,0};
+    int b[]={0,0,0,0,0,0,0,0,0};
+    int f=0;
+    for(int i=0;i<9;i++){
+    for(int j=0;j<9;j++){
+    f=sud[i][j];
+    if(f>0&&f<10){
+    a[f-1]=a[f-1]+1;
+    if(a[f-1]>1){
+    return 0;
+    }
+    }
+    else{
+    return 0;
+    }
+    } //for ends
+    a[0]=0;a[1]=0;a[2]=0;a[3]=0;a[4]=0;a[5]=0;a[6]=0;a[7]=0;a[8]=0;
+    } //for ends
+     
+    for(int i=0;i<9;i++){
+    for(int j=0;j<9;j++){
+    f=sud[j][i];
+    if(f>0&&f<10){
+    b[f-1]=b[f-1]+1;
+    if(b[f-1]>1){
+    return 0;
+    }
+    }
+    else{
+    return 0;
+    }
+    } //for ends
+    b[0]=0;b[1]=0;b[2]=0;b[3]=0;b[4]=0;b[5]=0;b[6]=0;b[7]=0;b[8]=0;
+    } //for ends
+    return 1;
+    }
