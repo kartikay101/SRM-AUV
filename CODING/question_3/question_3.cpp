2f07348bddffd66ac6f3597e3fc7442327381195
kartikay101 <shandilkartikay@gmail.com>
2016-08-28 07:47:12 +0530

    #3 comments added

--- CODING/question_3/question_3.cpp
+++ CODING/question_3/question_3.cpp
@@ -1,10 +1,11 @@
+//open README.txt before using the code
     #include<iostream>
-    void input();
-    void display();
-    int test();
+    void input();  //takes input
+    void display(); //displays input
+    int test(); //tests validiy
     using namespace std ;
      
-    int sud[9][9];
+    int sud[9][9];  //input sudoku
     int main()
     {
     int x=1;
@@ -47,7 +48,7 @@
     int a[]={0,0,0,0,0,0,0,0,0};
     int b[]={0,0,0,0,0,0,0,0,0};
     int f=0;
-    for(int i=0;i<9;i++){
+    for(int i=0;i<9;i++){ //checks rows for duplicates
     for(int j=0;j<9;j++){
     f=sud[i][j];
     if(f>0&&f<10){
@@ -60,10 +61,10 @@
     return 0;
     }
     } //for ends
-    a[0]=0;a[1]=0;a[2]=0;a[3]=0;a[4]=0;a[5]=0;a[6]=0;a[7]=0;a[8]=0;
+    a[0]=0;a[1]=0;a[2]=0;a[3]=0;a[4]=0;a[5]=0;a[6]=0;a[7]=0;a[8]=0;  //all array elements reset to 0;
     } //for ends
      
-    for(int i=0;i<9;i++){
+    for(int i=0;i<9;i++){  //checks columns for duplicates
     for(int j=0;j<9;j++){
     f=sud[j][i];
     if(f>0&&f<10){
@@ -76,7 +77,7 @@
     return 0;
     }
     } //for ends
-    b[0]=0;b[1]=0;b[2]=0;b[3]=0;b[4]=0;b[5]=0;b[6]=0;b[7]=0;b[8]=0;
+    b[0]=0;b[1]=0;b[2]=0;b[3]=0;b[4]=0;b[5]=0;b[6]=0;b[7]=0;b[8]=0; //all array elements reset to 0;
     } //for ends
     return 1;
     }
